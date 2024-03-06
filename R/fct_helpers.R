7659ae61a72962ae02477acfca67a70ec87bd21a
Neil Maginnis <neilm@ices.dk>
2024-03-06 15:02:11 +0100

    add helper functions to fix bugs

--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -1,6 +1,74 @@
 #' helpers 
 #' 
 #' 
+ac <- function(x, ...){
+  as.character(x, ...)
+}
+
+matrixIndex <- function(idx = NULL, coord = NULL, dim.mat = NULL){
+  if(is.null(idx) & is.null(coord) | is.null(dim.mat)){
+    stop("must supply either 'idx' or 'coord', and 'dim.mat'")
+  }
+  if(is.null(idx) & !is.null(coord) & !is.null(dim.mat)){
+    if(!is.matrix(coord)) coord <- matrix(coord, ncol = 2, byrow = TRUE)
+    idx <- ((coord[,2]-1)*dim.mat[1])+coord[,1] # position
+    return(idx)
+  }
+  if(!is.null(idx) & is.null(coord) & !is.null(dim.mat)){
+    coord <- matrix(NA, nrow = length(idx), ncol=2)
+    colnames(coord) <- c("row", "col")
+    coord[,1] <- ((idx-1) %% dim.mat[1]) +1
+    coord[,2] <- ((idx-1) %/% dim.mat[1]) +1
+    return(coord)
+  }
+}
+
+
+val2col<-function(z, zlim, col = hcl.colors(12, "YlOrRd", rev = TRUE), breaks){
+  if(!missing(breaks)){
+    if(length(breaks) != (length(col)+1)){stop("must have one more break than color")}
+  }
+  if(missing(breaks) & !missing(zlim)){
+    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
+  }
+  if(missing(breaks) & missing(zlim)){
+    zlim <- range(z, na.rm=TRUE)
+    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
+  }
+  CUT <- cut(z, breaks=breaks, include.lowest = TRUE)
+  colorlevels <- col[match(CUT, levels(CUT))] # assign colors to heights for each point
+  return(colorlevels)
+}
+
+
+matrixPoly <- function(x, y, z, n=NULL){
+  if(missing(z)) stop("Must define matrix 'z'")
+  if(missing(n)) n=seq(z)
+  if(missing(x)) x <- seq(0, 1, length.out = dim(z)[1])
+  if(missing(y)) y <- seq(0, 1, length.out = dim(z)[2])
+  poly <- vector(mode="list", length(n))
+  for(i in seq(length(n))){
+    ROW <- row(z)[n[i]] # ((n[i]-1) %% dim(z)[1]) +1
+    COL <- col(z)[n[i]] # ((n[i]-1) %/% dim(z)[1]) +1
+    
+    dist.left <- (x[ROW]-x[ROW-1])/2
+    dist.right <- (x[ROW+1]-x[ROW])/2
+    if(ROW==1) dist.left <- dist.right
+    if(ROW==dim(z)[1]) dist.right <- dist.left
+    
+    dist.down <- (y[COL]-y[COL-1])/2
+    dist.up <- (y[COL+1]-y[COL])/2
+    if(COL==1) dist.down <- dist.up
+    if(COL==dim(z)[2]) dist.up <- dist.down
+    
+    xs <- c(x[ROW]-dist.left, x[ROW]-dist.left, x[ROW]+dist.right, x[ROW]+dist.right, x[ROW]-dist.left)
+    ys <- c(y[COL]-dist.down, y[COL]+dist.up, y[COL]+dist.up, y[COL]-dist.down, y[COL]-dist.down)
+    poly[[i]] <- data.frame(x=xs, y=ys)
+  }
+  return(poly)
+}
+
+
 #' imageCor
 #'
 #' @description A plotting function for producing correlation plots
@@ -63,4 +131,3 @@ col = colorRampPalette(c(2,"white", 4))(21), zlim = c(-1,1),
        col = 1, font = labelFont, pos = 3)
   
 }
-
