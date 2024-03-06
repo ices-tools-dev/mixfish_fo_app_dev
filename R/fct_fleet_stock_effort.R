dd313314cdb9dcab9cceb1be357a1a10b5929733
Neil Maginnis <neilm@ices.dk>
2024-03-06 14:33:40 +0100

    Add plot for fleet fishing effort per stock

--- R/fct_fleet_stock_effort.R
+++ R/fct_fleet_stock_effort.R
@@ -1,13 +1,13 @@
 #' fleet_stock_effort 
 #'
-#' @description a function to calculate and plot the effort per fleet by stock
+#' @description a function to calculate and plot the relative effort per fleet by stock
 #' 
 #' @param df 
 #' @param eff 
 #' @param advice_year 
 #' @param upper_lim 
 #'
-compute_fleet_stock_effort <- function(df, eff, advice_year = NULL, upper_lim = 100) {
+compute_fleet_stock_effort <- function(df, eff, advice_year = NULL, upper_lim = 100, relative_effort = F) {
   
   if(is.null(advice_year)){
     advice_year <- lubridate::year(Sys.Date())-1
@@ -22,17 +22,44 @@ compute_fleet_stock_effort <- function(df, eff, advice_year = NULL, upper_lim = 
   eff <- merge(x = eff, y = sqEff, all.x = TRUE)
   df <- merge(x = df, y = eff, all.x = TRUE)
   df$quotaEffort <- df$effort / df$quotaUpt
-  df$relEffort <- df$quotaEffort / df$sqEffort
-  df$scenario <- df$stock
-  restr.stks <- c("COD-NS", "HAD", "PLE-EC", "PLE-NS", "POK", "SOL-EC",
-                  "SOL-NS", "TUR", "WHG-NS", "WIT", "NEP6", "NEP7", "NEP8", "NEP9")
-  df <- subset(df, stock %in% restr.stks)
   
-  # convert to percentage change
-  df$var <- 100*(df$relEffort-1)
+  restr.stks <- c("COD-NS", "HAD", "PLE-EC", "PLE-NS", "POK", "SOL-EC", "SOL-NS", "TUR", "WHG-NS", "WIT", "NEP6", "NEP7", "NEP8", "NEP9")
   
-  # optional upper limit (e.g. 100)
-  df$var <- ifelse(df$var > upper_lim, upper_lim, df$var)
-  return(df)
+  if(relative_effort == T) {
+    
+    df$relEffort <- df$quotaEffort / df$sqEffort
+    df$scenario <- df$stock
+    df2 <- subset(df, stock %in% restr.stks)
+    
+    # convert to percentage change
+    df2$var <- 100*(df2$relEffort-1)
+    
+    # optional upper limit (e.g. 100)
+    df2$var <- ifelse(df2$var > upper_lim, upper_lim, df2$var)
+  
+  } else if(relative_effort == F){
+    
+    fls <- unique(df$fleet)
+    df2 <- vector("list", length(fls))
+    names(df2) <- fls
+    for (i in seq(fls)) {
+      tmp <- subset(df, fleet == fls[i])
+      tmp$Limitation <- NA # initial NA setting for all stocks
+      mostLimStk <- subset(tmp, stock %in% restr.stks)
+      mostLimStk <- mostLimStk$stock[which.max(mostLimStk$quotaUpt)]
+      tmp$Limitation[which(tmp$stock == mostLimStk)] <- "most"
+      leastLimStk <- subset(stfFltStkSum, scenario == "max" & year == advYr &
+                              fleet == fls[i] & stock %in% restr.stks)
+      leastLimStk <- leastLimStk$stock[which.min(leastLimStk$quotaUpt)]
+      tmp$Limitation[which(tmp$stock == leastLimStk)] <- "least"
+      df2[[i]] <- tmp
+    }
+    df2 <- do.call("rbind", df2)
+    
+    # add Advice_name corresponding to refTable
+    df2 <- merge(x = df2, y = refTable[, c("stock", "Advice_name")], all.x = TRUE)
+    
+  }
+  return(df2)
 }
   
--- R/mod_fishing_effort.R
+++ R/mod_fishing_effort.R
@@ -12,8 +12,11 @@ mod_fishing_effort_ui <- function(id){
   ns <- NS(id)
   tagList(
     card(full_screen = T, fill = T, height = "90vh",
-      plotly::plotlyOutput(ns("fleet_stock_effort_plot"), height = "100%")
-         )
+      plotly::plotlyOutput(ns("relative_effort_fleet_stock_plot"), height = "100%")
+         ),
+    card(full_screen = T, fill = T, height = "90vh",
+      plotly::plotlyOutput(ns("fleet_stock_plot"), height = "100%")
+         ),
     )
 }
     
@@ -22,7 +25,7 @@ mod_fishing_effort_ui <- function(id){
 #' @noRd 
 #' @importFrom ggplot2 theme element_text coord_cartesian
 #' @importFrom plotly ggplotly renderPlotly layout
-#' @importFrom mixfishtools plot_relEffortFltStk
+#' @importFrom mixfishtools plot_relEffortFltStk plot_effortFltStk
 #' 
 mod_fishing_effort_server <- function(id, region){
   moduleServer( id, function(input, output, session){
@@ -35,11 +38,25 @@ mod_fishing_effort_server <- function(id, region){
            fleet_sum = fleet_sum)
     })
     
-    output$fleet_stock_effort_plot <- renderPlotly({
+    output$relative_effort_fleet_stock_plot <- renderPlotly({
       
-      df <- compute_fleet_stock_effort(df = data_effort_reactive()$fleet_stock_sum, eff = data_effort_reactive()$fleet_sum)
+      df <- compute_fleet_stock_effort(df = data_effort_reactive()$fleet_stock_sum, eff = data_effort_reactive()$fleet_sum, relative_effort = T)
       
       plot <- plot_relEffortFltStk(data = df) + 
+        theme(axis.text.x = element_text(angle = 0, hjust = 0, vjust = 0.3),
+              text = element_text(size = 13)) +
+        coord_cartesian() 
+      
+      plot %>% ggplotly() %>% 
+        layout(autosize = TRUE)
+    })
+    
+    
+    output$fleet_stock_plot <- renderPlotly({
+      
+      df <- compute_fleet_stock_effort(df = data_effort_reactive()$fleet_stock_sum, eff = data_effort_reactive()$fleet_sum, relative_effort = F)
+      
+      plot <- plot_effortFltStk(data = df, refTable = refTable) + 
         theme(axis.text.x = element_text(angle = 0, hjust = 0, vjust = 0.3),
               text = element_text(size = 13)) +
         coord_cartesian() 
